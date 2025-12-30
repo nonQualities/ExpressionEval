7261453a4e65960a086eaa8a2f656b270782a61b
nonQualities <ronit965choudhury@outlook.com>
2025-12-30 13:40:33 +0530

    fixed errors in evaluator and parser

--- app/Eval.hs
+++ app/Eval.hs
@@ -5,16 +5,17 @@ module Eval (
 ) where
 
 import AST
-import Control.Exception
 
-data Val 
+data Val
     = ValInt Int
     | ValBool Bool
     deriving (Eq, Show)
 
 type Env = [(Vname, Val)]
 
-envEmpty::Env
+type Error = String
+
+envEmpty :: Env
 envEmpty = []
 
 envExtend :: Vname -> Val -> Env -> Env
@@ -23,55 +24,58 @@ envExtend v val env = (v,val) : env
 envLookup :: Vname -> Env -> Maybe Val
 envLookup = lookup
 
-evalIntBinOp :: (Int -> Int -> Either Error Int) -> Exp -> Exp -> Either Error Val
-evalIntBinOp f eval e1 e2 =
-    case (eval e1, eval e2) of
+-- 1. Helper for operations that MIGHT fail (like Div)
+-- Returns Either Error Int, which we then wrap in ValInt
+evalIntBinOp :: (Int -> Int -> Either Error Int) -> Env -> Exp -> Exp -> Either Error Val
+evalIntBinOp f env e1 e2 =
+    case (eval env e1, eval env e2) of
         (Left err, _) -> Left err
         (_, Left err) -> Left err
-        (Right(ValInt x), Right(ValInt y)) -> case f x y of
-            Left err -> Left err
-            Right z -> Right $ ValInt z
-        (Right _ , Right _) -> Left "Non integer operand"
+        (Right (ValInt x), Right (ValInt y)) ->
+            case f x y of
+                Left err -> Left err
+                Right z  -> Right $ ValInt z
+        (Right _, Right _) -> Left "Type Error: Non-integer operand in integer operation"
 
+-- 2. Helper for operations that CANNOT fail (like Add, Mul)
+-- Wraps the simple Int -> Int -> Int function into the error-handling type
 evalIntBinOp' :: (Int -> Int -> Int) -> Env -> Exp -> Exp -> Either Error Val
-evalIntBinOp' f env e1 e2 =
-    evalIntBinOp f' env e1 e2
-    where 
-        f' :: Int -> Int -> Either Error Int
-        f' x y = Right $ f x y
-
-type Error = String
+evalIntBinOp' f = evalIntBinOp (\x y -> Right (f x y))
 
 eval :: Env -> Exp -> Either Error Val
-eval _ env (CnstInt x) = Right $ ValInt x
-eval _ env (CnstBool x) = Right $ ValBool b
+-- Fixed: Removed the extra '_' argument to match type signature
+eval _ (CnstInt x) = Right $ ValInt x
+eval _ (CnstBool x) = Right $ ValBool x
 eval env (Var v) = case envLookup v env of
-    Just x -> Right x
-    Nothing -> Left $ "Unkown variable: " ++ v
-eval _ env (Add e1 e2) = evalIntBinOp' (+) env e1 e2
-eval _ env (Sub e1 e2) = evalIntBinOp' (-) env e1 e2
-eval _ env (Mul e1 e2) = evalIntBinOp' (*) env e1 e2
-eval _ env (Div e1 e2) = evalIntBinOp' checkdiv env e1 e2
+    Just x  -> Right x
+    Nothing -> Left $ "Unknown variable: " ++ v
+
+-- Binary Operations
+eval env (Add e1 e2) = evalIntBinOp' (+) env e1 e2
+eval env (Sub e1 e2) = evalIntBinOp' (-) env e1 e2
+eval env (Mul e1 e2) = evalIntBinOp' (*) env e1 e2
+
+-- Div needs the error-checking helper (evalIntBinOp), not the safe one
+eval env (Div e1 e2) = evalIntBinOp safeDiv env e1 e2
+  where
+    safeDiv :: Int -> Int -> Either Error Int
+    safeDiv _ 0 = Left "Division by zero"
+    safeDiv x y = Right (x `div` y)
+
+eval env (Pow e1 e2) = evalIntBinOp safePow env e1 e2
     where
-        checkdiv :: Int -> Int -> Either Error Val
-        checkdiv _ 0 = Left "Division by zero not possible"
-        checkdiv x y = Right $ x `div` y
-eval env (Pow e1 e2) = evalIntBinOp checkPow env e1 e2
-    where
-        checkPow x y =
-            if y < 0 then
-                Left "Negative Exponent"
-            else
-                Right $ x ^ y
+        safePow :: Int -> Int -> Either Error Int
+        safePow x y = if y < 0 then Left "Negative Exponent" else Right (x ^ y)
+
 eval env (Eql e1 e2) =
-    case(eval env e1, eval env e2) of
+    case (eval env e1, eval env e2) of
         (Left err, _) -> Left err
         (_, Left err) -> Left err
-        (Right (ValInt x), Right (ValInt y)) -> Right $ ValBool $ x ==y
-        (Right _, Right _) -> Left "Invalid operands to equality"
+        (Right (ValInt x), Right (ValInt y)) -> Right $ ValBool (x == y)
+        (Right (ValBool x), Right (ValBool y)) -> Right $ ValBool (x == y) -- Added Bool equality for completeness
+        (Right _, Right _) -> Left "Type Error: Invalid operands to equality"
+
 eval env (Let var e1 e2) =
     case eval env e1 of
         Left err -> Left err
-        Right v -> eval (envExtend var v env) e2
-
-
+        Right v  -> eval (envExtend var v env) e2
--- app/Frontend/Parser.y
+++ app/Frontend/Parser.y
@@ -5,58 +5,56 @@ import System.IO
 import AST
 }
 
-%name Parser
-%tokentype {Token}
+%name parse
+%tokentype { Token }
 %error { parseError }
 
 %token
-id           {ID $$ }
-int          {INT $$}
-bool         {BOOL $$}
-minus        {MINUS}
-plus         {PLUS}
-multiply     {MULTIPLY}
-divide       {DIVIDE}
-power        {POWER}
-eql          {EQUALITY}
-eq           {EQUALS}
-in           {IN}
-let          {LET}
 
+id           { ID $$ }
+int          { INT $$ }
+bool         { BOOL $$ }
+minus        { MINUS }
+plus         { PLUS }
+multiply     { MULTIPLY }
+divide       { DIVIDE }
+power        { POWER }
+eq           { EQUALS }
+eql          { EQUALITY }
+in           { IN }
+let          { LET }
 
-
-%nonassoc id eq 
+%nonassoc id eq
 %left minus plus
-%left divide multiply
+%left divide mulitply
 %right power eql
-
 
 %%
 
---production
 expr :: { Exp }
-    : expr plus expr            {Add $1 $3}
-    | expr minus expr           {Sub $1 $3}
-    | expr multiply expr        {Multiply $1 $3}
-    | expr divide expr          {Divide $1 $3}
-    | expr power expr           {Power $1 $3}
-    | expr eql expr             {Eql $1 $3}
-    | int                       {CstInt $1}
-    | let id eq expr in expr    {Let $2 $4 $6}
-    | id                        {Var $1}
-    | bool                      {CstBool $1}
-
+  : expr plus expr          { Add $1 $3 }
+  | expr minus expr         { Sub $1 $3 }
+  | expr multiply expr      { Mul $1 $3 }
+  | expr divide expr        { Div $1 $3 }
+  | expr power expr         { Pow $1 $3 }
+  | expr eql expr           { Eql $1 $3}
+  | int                     { CnstInt $1 }
+  | let id eq expr in expr  { Let $2 $4 $6 }
+  | id                      { Var $1 }
+  | bool                    { CnstBool $1 }
 
 {
 parseError :: [Token] -> a
-parseError [] = error "ParseError: Empty token stream"
-parseError (tok:_) error $ "ParseError: Unexpected token  "++show tok ++ "."
+parseError [] = error "ParseError: Empty token stream."
+parserError (tok:_) = error $ "ParseError: Unexpected token '" ++ show tok ++ "'."
 
 parseString :: String -> Exp
 parseString = parse . scan
 
 parseFile :: FilePath -> IO Exp
-parseFile f = return . parseString
 parseFile f = return . parseString =<< readFile f
+-- parseFile f = do
+--     s <- readFile f -- String
+--     let res = parseString s
+--     return res
 }
-
